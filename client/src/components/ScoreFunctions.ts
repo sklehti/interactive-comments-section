9289228d85384b5a8341aa88891f5319c96b4b83
Saara Lehtinen <saaralehtinen77@gmail.com>
2023-12-08 11:02:39 +0200

    app ready for production

--- client/src/components/ScoreFunctions.ts
+++ client/src/components/ScoreFunctions.ts
@@ -29,28 +29,103 @@ export const handlePlusScore = ({
 }: Props) => {
   let newScore: Score;
 
-  if (event.currentTarget.value !== currentUser?.username) {
-    if (currentUser?.user_id) {
-      if ("id" in reply && reply.id !== undefined) {
-        newScore = {
-          comment_id: reply.id,
-          user_id: currentUser?.user_id,
-          comment_type: "r",
-        };
-      } else {
-        newScore = {
-          comment_id: reply.comment_id,
-          user_id: currentUser?.user_id,
-          comment_type: "c",
-        };
-      }
+  // Note! add the following if-condition if you want to remove the option to add a point to your comment
+  // if (event.currentTarget.value !== currentUser?.username) {
+  if (currentUser?.user_id) {
+    if ("id" in reply && reply.id !== undefined) {
+      newScore = {
+        comment_id: reply.id,
+        user_id: currentUser?.user_id,
+        comment_type: "r",
+      };
+    } else {
+      newScore = {
+        comment_id: reply.comment_id,
+        user_id: currentUser?.user_id,
+        comment_type: "c",
+      };
+    }
 
-      const createdScore = toNewScore(newScore);
+    const createdScore = toNewScore(newScore);
 
-      scores(createdScore)
-        .then((response) => {
-          if (response.length === 0) {
-            createScore(newScore).then(() => {
+    scores(createdScore)
+      .then((response) => {
+        if (response.length === 0) {
+          createScore(newScore).then(() => {
+            if (createdScore.comment_type === "c") {
+              getComments()
+                .then((response) => {
+                  setAllComments(response);
+                })
+                .catch((error) => {
+                  setErrorMessage(parseError(error));
+                  setTimeout(() => {
+                    setErrorMessage("");
+                  }, 3000);
+                });
+            } else {
+              getReplies()
+                .then((response) => {
+                  setReplies(response);
+                })
+                .catch((error) => {
+                  setErrorMessage(parseError(error));
+                  setTimeout(() => {
+                    setErrorMessage("");
+                  }, 3000);
+                });
+            }
+          });
+        } else {
+          alert("you have already given a point to a comment!");
+        }
+      })
+      .catch((error) => {
+        setErrorMessage(parseError(error));
+        setTimeout(() => {
+          setErrorMessage("");
+        }, 3000);
+      });
+  }
+  // } else {
+  //   alert("You cannot give points for your own comment!");
+  // }
+};
+
+export const handleMinusScore = ({
+  reply,
+  currentUser,
+  setAllComments,
+  setReplies,
+  event,
+  setErrorMessage,
+}: Props) => {
+  let score: Score;
+
+  // Note! add the following if-condition if you want to remove the option to remove a point to your comment
+  // if (event.currentTarget.value !== currentUser?.username) {
+  if (currentUser?.user_id) {
+    if ("id" in reply && reply.id !== undefined) {
+      score = {
+        comment_id: reply.id,
+        user_id: currentUser?.user_id,
+        comment_type: "r",
+      };
+    } else {
+      score = {
+        comment_id: reply.comment_id,
+        user_id: currentUser?.user_id,
+        comment_type: "c",
+      };
+    }
+
+    const createdScore = toNewScore(score);
+
+    scores(createdScore)
+      .then((response) => {
+        if (response.length > 0) {
+          updateScore(score)
+            .then(() => {
               if (createdScore.comment_type === "c") {
                 getComments()
                   .then((response) => {
@@ -74,100 +149,27 @@ export const handlePlusScore = ({
                     }, 3000);
                   });
               }
+            })
+            .catch((error) => {
+              setErrorMessage(parseError(error));
+              setTimeout(() => {
+                setErrorMessage("");
+              }, 3000);
             });
-          } else {
-            alert("you have already given a point to a comment!");
-          }
-        })
-        .catch((error) => {
-          setErrorMessage(parseError(error));
-          setTimeout(() => {
-            setErrorMessage("");
-          }, 3000);
-        });
-    }
-  } else {
-    alert("You cannot give points for your own comment!");
+        } else {
+          alert(
+            "You haven't given a point to the comment so you can't remove the point!"
+          );
+        }
+      })
+      .catch((error) => {
+        setErrorMessage(parseError(error));
+        setTimeout(() => {
+          setErrorMessage("");
+        }, 3000);
+      });
   }
-};
-
-export const handleMinusScore = ({
-  reply,
-  currentUser,
-  setAllComments,
-  setReplies,
-  event,
-  setErrorMessage,
-}: Props) => {
-  let score: Score;
-
-  if (event.currentTarget.value !== currentUser?.username) {
-    if (currentUser?.user_id) {
-      if ("id" in reply && reply.id !== undefined) {
-        score = {
-          comment_id: reply.id,
-          user_id: currentUser?.user_id,
-          comment_type: "r",
-        };
-      } else {
-        score = {
-          comment_id: reply.comment_id,
-          user_id: currentUser?.user_id,
-          comment_type: "c",
-        };
-      }
-
-      const createdScore = toNewScore(score);
-
-      scores(createdScore)
-        .then((response) => {
-          if (response.length > 0) {
-            updateScore(score)
-              .then(() => {
-                if (createdScore.comment_type === "c") {
-                  getComments()
-                    .then((response) => {
-                      setAllComments(response);
-                    })
-                    .catch((error) => {
-                      setErrorMessage(parseError(error));
-                      setTimeout(() => {
-                        setErrorMessage("");
-                      }, 3000);
-                    });
-                } else {
-                  getReplies()
-                    .then((response) => {
-                      setReplies(response);
-                    })
-                    .catch((error) => {
-                      setErrorMessage(parseError(error));
-                      setTimeout(() => {
-                        setErrorMessage("");
-                      }, 3000);
-                    });
-                }
-              })
-              .catch((error) => {
-                setErrorMessage(parseError(error));
-                setTimeout(() => {
-                  setErrorMessage("");
-                }, 3000);
-              });
-          } else {
-            alert(
-              "You haven't given a point to the comment so you can't remove the point!"
-            );
-          }
-        })
-        .catch((error) => {
-          setErrorMessage(parseError(error));
-          setTimeout(() => {
-            setErrorMessage("");
-          }, 3000);
-        });
-    }
-  } else {
-    alert("You cannot remove points for your own comment!");
-  }
+  // } else {
+  //   alert("You cannot remove points for your own comment!");
+  // }
 };
